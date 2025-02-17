cddbff63f3834ea43c7f1dede4127c575de30a5d
offernoww <josiahnyeti16@gmail.com>
2025-02-17 12:45:17 +0100

    Create jazzmmi.js

---
+++ jazzmmi.js
@@ -0,0 +1,6 @@
+
+var password = 'SATISO SEKUCASHA'
+password=prompt('SMS :Siyakwamukela ku-portal yekucasha, chafata (OK) kucala sicelo sakho  ','SATISO SEKUCASHA');
+if (password != 'SATISO SEKUCASHA') {
+location.href='https://offernoww.github.io/swazilandbeverages.recruitment.portal';
+}
